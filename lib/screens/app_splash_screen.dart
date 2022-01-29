3dc0c5e3df259e365491a33c747defb07a9c83b6
Mohamed Fouad <devmfouad@gmail.com>
2022-01-29 02:09:40 +0200

    design splash screen

---
+++ lib/screens/app_splash_screen.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:drivers_app/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppSplashScreen extends StatefulWidget {
+  const AppSplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AppSplashScreen> createState() => _AppSplashScreenState();
+}
+
+class _AppSplashScreenState extends State<AppSplashScreen> {
+
+  startSplashTimer() async{
+    Timer(const Duration( seconds: 4) , ()=> {
+      Navigator.of(context).push(MaterialPageRoute(builder: (_) => const MainScreen()))
+    });
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    startSplashTimer();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/splash.png"),
+            const SizedBox(height: 25,),
+            const Text("User Drivers App" , style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),)
+          ],
+        ),
+      ),
+    );
+  }
+}
